186baa5a0023ac7df690954136b715bedcb55d0d
Leon Sorokin <leeoniya@gmail.com>
2013-01-27 01:08:28 -0600

    pxTrcr rewrite (unifies buffering + recording)

--- src/pxTrcr.js
+++ src/pxTrcr.js
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2012, Leon Sorokin
+* Copyright (c) 2013, Leon Sorokin
 * All rights reserved. (MIT Licensed)
 *
 * pxTrcr.js - pixel tracer for pXY.js
@@ -8,40 +8,29 @@
 function pxTrcr(w, h, ctnr) {
 	this.w = w;
 	this.h = h;
-	this.record = false;
-	this.buffer = false;
-	this.timeout = 1000/60;
-	this.queue = null;
 	this.ctnr = ctnr || null;
-	this.buf = null;
 
-	// layer stack
+	// layer/px/chkr combos
+	this.cfgs = [];
 	this.lyrs = {};
-	// state stack
-	this.hist = [];
+
+	// recording & buffering
+	this._rec = false;
+	this.buf = [];
+	this.rid = null;		// id returned by reqAnimFrame
+
+	if (!this.ctnr)
+		this.rec();
 
 	// init layer 0 and trans pixel
 	this.push([0,0,0,0], 0);
 }
 
 (function() {
-	function q() {
-		var self = this;
-		this.items = [];
-
-		this.enq = function enq(fn) {
-			this.items.push(fn);
-		};
-
-		this.deq = function deq() {
-			this.items.length && (this.items.shift()).call(self);
-		};
-	}
-
-	function lyr(w, h, id) {
+	function Lyr(w, h, id) {
 		this.id = id;
 
-		this.can = document.createElement('canvas');
+		this.can = document.createElement("canvas");
 		this.can.id = "trclyr-" + id;
 		this.can.className = "trclyr";
 		this.can.width = w;
@@ -51,7 +40,7 @@ function pxTrcr(w, h, ctnr) {
 		this.can.style.top = 0;
 		this.can.style.background = "transparent";
 
-		this.ctx = this.can.getContext('2d');
+		this.ctx = this.can.getContext("2d");
 		this.imgd = this.ctx.createImageData(w, h);
 		this.pxls = this.imgd.data;
 		this.dirty = false;
@@ -60,7 +49,7 @@ function pxTrcr(w, h, ctnr) {
 			buf8 = new Uint8Array(buf),
 			buf32 = new Uint32Array(buf);
 
-		this.setPx = function setPxLyr(i,px) {
+		this.setPx = function lyrSetPx(i,px) {
 			px[3] = px[3] || 255;
 			buf32[i] =
 				(px[3] << 24) |	// alpha
@@ -71,44 +60,17 @@ function pxTrcr(w, h, ctnr) {
 			this.dirty = true;
 		};
 
-		this.upd = function updLyr() {
+		this.upd = function lyrUpd() {
 			if (!this.dirty) return;
 
 			this.imgd.data.set(buf8);
 			this.ctx.putImageData(this.imgd, 0, 0);
 			this.dirty = false;
 		};
-
-		this.clr = function clrLyr() {
-			this.ctx.clearRect(0, 0, this.can.width, this.can.height);
-			this.imgd = this.ctx.getImageData(0, 0, this.can.width, this.can.height);
-			this.pxls = this.imgd.data;
-			this.dirty = false;
-		};
-	}
-
-	function lyrFaux(w, h, id) {
-		this.id = id;
-	}
-
-	// get and/or make a layer
-	function lyrProduce(id) {
-		if (this.lyrs[id])
-			return this.lyrs[id];
-
-		// return a fake, mock layer when in a worker context
-		if (typeof window === "undefined")
-			this.lyrs[id] = new lyrFaux(this.w, this.h, id);
-		else
-			this.lyrs[id] = new lyr(this.w, this.h, id);
-
-		this.ctnr && this.ctnr.appendChild(this.lyrs[id].can);
-
-		return this.lyrs[id];
 	}
 
 	// one-shot (single move or scan)
-	function One() {
+	function OneChkr() {
 		this.fired = true;
 
 		this.chk = function(type, id) {
@@ -124,7 +86,7 @@ function pxTrcr(w, h, ctnr) {
 	}
 
 	// sticky
-	function Set() {
+	function SetChkr() {
 		this.chk = function(type, id) {
 			return true;
 		}
@@ -132,152 +94,108 @@ function pxTrcr(w, h, ctnr) {
 
 	// modules
 	var mods = {
-		queue: {
-			// enable recording
-			recOn: function rec() {
-				this.record = true;
-				this.queue = new q;
-
+		buffer: {
+			rec: function rec() {
+				return this.recOn();
+			},
+			recOn: function recOn() {
+				this._rec = true;
 				return this;
 			},
-			recOff: function rec() {
-				this.record = false;
-				this.queue = null;
-
+			recOff: function recOff() {
+				this._rec = false;
 				return this;
 			},
-			bufOn: function() {
-				this.buffer = true;
-				this.buf = [];
-				!this.lyrs.length && this.push([0,0,0,0], 0);
-
-				return this;
+			enq: function enq(fnIdx, args) {
+				this.buf.push(Array.prototype.slice.call(arguments));
 			},
-			bufOff: function() {
-				this.buffer = false;
-				this.buf = null;
+			deq: function deq() {
+				var op = this.buf.shift();
+				this[op[0]].apply(this, op[1]);
 
-				return this;
-			},
-			bufGet: function(clear) {
-				var buf = this.buf;
-
-				if (clear)
-					this.buf = [];
-
-				return buf;
-			},
-			bufRun: function(buf) {
-				for (var i in buf)
-					this[buf[i][0]].apply(this, buf[i][1]);
-
-				return this;
-			},
-			// queues or executes functions
-			exec: function exec(fn, fnName, args, timeout) {
-				if (this.buffer) {
-					if (fnName == "notify") {			// move this out?
-						var evt = args[0];
-					//	evt.i = evt.pxy.absIdx();		// abs here, but relative x/y?
-						evt.x = evt.pxy.x;
-						evt.y = evt.pxy.y;
-						delete evt.pxy;
-					}
-					else
-						args = Array.prototype.slice.apply(args);
-
-					this.buf.push([fnName,args]);
+				return op;						// hack to allow introspection
+			}
+		},
+		stack: {
+			// @params: pxl, lyrId
+			set:  function set()  {return this.cfgOp(3, arguments);},
+			push: function push() {return this.cfgOp(4, arguments);},
+			pop:  function pop()  {return this.cfgOp(5, arguments);},
+			one:  function one()  {return this.cfgOp(6, arguments);},
+			// DRY base method
+			cfgOp: function cfgOp(fnIdx, args) {
+				if (this._rec) {
+					this.enq(fnIdx, args);
 					return this;
 				}
 
-				if (!this.record)
-					return fn.apply(this, args);
+				if (fnIdx !== 5)
+					var pxLyr = this.pxLyr.apply(this, args);
 
-				var self = this;
-				self.queue.enq(function() {
-					window.setTimeout(function() {
-						fn.apply(self, args);
-						self.queue.deq();
-					}, timeout || timeout === 0 ? timeout : self.timeout);
-				});
-			},
-		},
-		stack: {
-			set: function set(pxl, lyrId) {
-				function go(pxl, lyrId) {
-					var pxLyr = this.pxLyrPair.apply(this, arguments);
-					this.hist[0] = [new Set].concat(pxLyr);
+				switch (fnIdx) {
+					case 3: this.cfgs[0] = [new SetChkr].concat(pxLyr);		break;
+					case 4: this.cfgs.unshift([new SetChkr].concat(pxLyr));	break;
+					case 5: this.cfgs.shift();								break;
+					case 6: this.cfgs.unshift([new OneChkr].concat(pxLyr));	break;
 				}
-
-				this.exec(go, "set", arguments, 0);
-
-				return this;
-			},
-			push: function push(pxl, lyrId) {
-				function go(pxl, lyrId) {
-					var pxLyr = this.pxLyrPair.apply(this, arguments);
-					this.hist.unshift([new Set].concat(pxLyr));
-				}
-
-				this.exec(go, "push", arguments, 0);
-
-				return this;
-			},
-			pop: function pop(now) {
-				if (now)
-					this.hist.shift();
-				else
-					this.exec(this.pop, "pop", [true], 0);
-
-				return this;
-			},
-			one: function one(pxl, lyrId) {
-				function go(pxl, lyrId) {
-					var pxLyr = this.pxLyrPair.apply(this, arguments);
-					this.hist.unshift([new One].concat(pxLyr));
-				}
-
-				this.exec(go, "one", arguments, 0);
 
 				return this;
 			},
 			// takes 1 or 2 args, layer id and/or pixel, in any order
-			pxLyrPair: function(a1, a2) {
+			pxLyr: function(a1, a2) {
 				function isLyrId(v) {
 					return typeof v == "string" || typeof v == "number";
 				}
 
 				switch (arguments.length) {
 					case 0:
-						return [this.hist[0][1], this.hist[0][2]];	// inherits both
+						return [this.cfgs[0][1], this.cfgs[0][2]];	// inherits both
 					case 1:
 						if (isLyrId(a1))
-							return [this.hist[0][1], this.lyr(a1, this.buffer)];	// get/make layer, inherit px
-						return [a1, this.hist[0][2]];				// use px, inherit layer
+							return [this.cfgs[0][1], this.lyr(a1)];	// get/make layer, inherit px
+						return [a1, this.cfgs[0][2]];				// use px, inherit layer
 					case 2:
 						if (isLyrId(a1))
-							return [a2, this.lyr(a1, this.buffer)];				// get/make layer, use px
-						return [a1, this.lyr(a2, this.buffer)];					// use px, get/make layer
+							return [a2, this.lyr(a1)];				// get/make layer, use px
+						return [a1, this.lyr(a2)];					// use px, get/make layer
 				}
-			},
+			}
 		},
 		layer: {
-			lyr: function lyr(id) {
-				return this.exec(lyrProduce, "lyr", arguments);
+			// get and/or make a layer
+			lyr: function lyr(lyrId) {
+				if (this.lyrs[lyrId])
+					return this.lyrs[lyrId];
+
+				this.lyrs[lyrId] = new Lyr(this.w, this.h, lyrId);
+
+				this.ctnr.appendChild(this.lyrs[lyrId].can);
+
+				return this.lyrs[lyrId];
 			},
 			clr: function clr(lyrId) {
-				if (lyrId && this.lyrs[lyrId])
-					this.lyrs[lyrId].clr();
+				if (lyrId || lyrId === 0) {
+					this.ctnr.removeChild(this.lyrs[lyrId].can);
+					delete this.lyrs[lyrId];
+
+					if (lyrId === 0)
+						this.push([0,0,0,0], 0);
+				}
 				else {
-					for (var i in this.lyrs)
-						this.lyrs[i].clr();
+					for (var i in this.lyrs) {
+						this.ctnr.removeChild(this.lyrs[i].can);
+						delete this.lyrs[i];
+					}
+
+					this.push([0,0,0,0], 0);
 				}
 
 				return this;
 			},
-			// draw pixel to layer at idx
-			setPx: function setPx(i, lyrId) {		// TODO, trace to inactive layer by id (good for async/web-workers)
-				var top = this.hist[0];
+			// draw current pixel to current layer at idx
+			// TODO: add lyrId to trace to inactive layer by id (good for async/web-workers)
+			setPx: function setPx(i) {
+				var top = this.cfgs[0];
 				top[2].setPx(i, top[1]);
 			},
 			// output pixels to canvas(es)
@@ -289,34 +207,65 @@ function pxTrcr(w, h, ctnr) {
 
 				return this;
 			},
-			// returns array with layer canvas(es)
-			cans: function get(lyrId) {
-				var lyrs = [];
-				for (var i in this.lyrs) {
-					if (lyrId && i != lyrId) continue;
-					lyrs.push(this.lyrs[i].can);
-				}
+			// @rate: ops/sec to dequeue from buffer
+			draw: function draw(rate) {
+				var recOld = this._rec;
+				this._rec = false;
 
-				return lyrs;
-			},
-			// .upd() + .cans()
-			rend: function rend(lyrId) {
-				this.upd.apply(this, arguments);
-				return this.cans.apply(this, arguments);
-			},
-			draw: function draw(timeout) {
-				if (this.queue && this.queue.items.length) {
-					if (timeout)
-						this.timeout = timeout;
-					this.queue.deq();
+				if (!this.buf.length) {
+					this.upd();						// just update layers
+					this._rec = recOld;
 				}
 				else {
-					var cans = this.rend();
-					for (var i in cans)
-						this.ctnr && this.ctnr.appendChild(cans[i]);
-				}
+					if (rate) {
+						var fps = 60,				// assumed max fps
+							ratio = rate / fps,		// target ratio
+							frame = 0,				// frame counter
+							opers = 0,				// op counter
+							self = this;
 
-				return this;
+						var step = function() {
+							if (!self.buf.length) {
+								self._rec = recOld;
+								self.rid = null;
+								return;
+							}
+
+							self.rid = requestAnimationFrame(step);
+
+							/*
+							if (frame == fps) {
+								frame = 0;
+								opers = 0;
+							}
+							*/
+
+							frame++;
+
+							var fnIdx, frOps = 0;
+							while (opers / frame < ratio && self.buf.length) {
+								fnIdx = self.deq()[0];
+
+								if (fnIdx == 0) {
+									frOps++;
+									opers++;
+								}
+							}
+
+							if (frOps)
+								self.upd();
+						};
+
+						self.rid = requestAnimationFrame(step);
+					}
+					else {
+						while (this.buf.length)
+							this.deq();
+
+						this.upd();
+						this._rec = recOld;
+					}
+				}
 			},
 		},
 		pubsub: {
@@ -328,19 +277,39 @@ function pxTrcr(w, h, ctnr) {
 				pxy.unsub(this.notify, this);
 				return this;
 			},
-			handle: function handle(evt) {
-				evt.type == 0 && this.setPx(evt.i);
-
-				this.record && this.upd();		// throttle?
-
-				if (!this.hist[0][0].chk(evt.type, evt.id || null))
-					this.pop(true);
-			},
 			notify: function notify(evt) {
-				if (!evt.i && evt.i !== 0) evt.i = evt.pxy.absIdx();			// will fail for i = 0?
-				this.exec(this.handle, "notify", [evt]);
-			},
+				var crd = evt.pxy.absXy(),
+					idx = evt.pxy.absIdx(),
+					args = [crd.x, crd.y, idx];
+
+				if (evt.id)
+					args.push(evt.id);
+
+				return this[evt.type].apply(this, args);
+			}
 		},
+		events: {
+			// @params: x, y, i, id
+			move:	function move()  {return this.evtOp(0, arguments);},
+			scan0:	function scan0() {return this.evtOp(1, arguments);},
+			scan1:	function scan1() {return this.evtOp(2, arguments);},
+			// DRY base method
+			evtOp: function evtOp(fnIdx, args) {
+				if (this._rec) {
+					this.enq(fnIdx, args);
+					return this;
+				}
+
+				// set pixel for moves
+				if (fnIdx == 0)
+					this.setPx(args[2]);
+
+				if (!this.cfgs[0][0].chk(fnIdx, args[3]))
+					this.pop();
+
+				return this;
+			}
+		}
 	};
 
 	// combine modules into proto
@@ -349,4 +318,9 @@ function pxTrcr(w, h, ctnr) {
 			pxTrcr.prototype[j] = mods[i][j];
 		}
 	}
+
+	// alias ops
+	var ops = "move scan0 scan1 set push pop one".split(" ");
+	for (var k in ops)
+		pxTrcr.prototype[k] = pxTrcr.prototype[ops[k]];
 })();
